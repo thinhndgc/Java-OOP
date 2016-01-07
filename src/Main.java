b3deae5403996beaa1dc181b007418db20fd1503
thinhndgc <ndthinh94@gmail.com>
2016-01-07 15:51:02 +0700

    edit method name

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
 	public static void createStudent(){
 		Students st = new Students("male", 21, "Nguyen Duc Thinh", "09-10-1994", "Ha Long", "Ha Noi", 1676564864, "thinhndgc00458@fpt.edu.vn", "SE", "FPT University");
 		System.out.println("Student " + st.getName() + "has created!");
-		checkGenser(st);
+		checkGender(st);
 		classRegister(st, "Android");
 	}
 	
-	public static void checkGenser(Students st){
+	public static void checkGender(Students st){
 		System.out.println("Gender of this student is: " + st.getGender());
 	}
 	
